c98388a290c31647054abd5a29ba2fce7f93b096
Resul Dinc <dincresull@gmail.com>
2024-07-23 22:38:55 +0300

    configure console app

--- FormulaAirline.TicketProcessing/Program.cs
+++ FormulaAirline.TicketProcessing/Program.cs
@@ -1,2 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RabbitMQ.Client;
+using System.Text.Json;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+Console.WriteLine("Welcome to the ticketing service");
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    UserName = "user",
+    Password = "mypass",
+    VirtualHost = "/"
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "bookings",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine(message);
+};
+
+channel.BasicConsume(queue: "bookings", true, consumer);
+
+Console.ReadKey();
