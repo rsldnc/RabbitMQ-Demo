db847669fc5f440fd81be514bd1dcd8c6935ce1c
Resul Dinc <dincresull@gmail.com>
2024-07-23 20:56:11 +0300

    create bookings controller

---
+++ FormulaAirline.API/Controllers/BookingsController.cs
@@ -0,0 +1,37 @@
+﻿using FormulaAirline.API.Models;
+using FormulaAirline.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormulaAirline.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingsController : ControllerBase
+    {
+        private readonly ILogger<BookingsController> _logger;
+        private readonly IMessageProducer _messageProducer;
+
+        // InMemoryDatabase
+        public static readonly List<Booking> _bookings = new List<Booking>();
+
+        public BookingsController(ILogger<BookingsController> logger, IMessageProducer messageProducer)
+        {
+            _logger = logger;
+            _messageProducer = messageProducer;
+        }
+
+        [HttpPost]
+        public IActionResult CreatingBooking(Booking newBooking)
+        {
+            if(ModelState.IsValid)
+                return BadRequest();
+            
+            _bookings.Add(newBooking);
+
+            _messageProducer.SendingMessage<Booking>(newBooking);
+
+            return Ok();
+        }
+    }
+}
