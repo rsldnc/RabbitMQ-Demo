7102d3a3027241ef23616edf3510dbe7f6d1979c
Resul Dinc <dincresull@gmail.com>
2024-07-23 20:19:51 +0300

    create booking model

---
+++ FormulaAirline.API/Models/Booking.cs
@@ -0,0 +1,12 @@
+﻿namespace FormulaAirline.API.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public string PassengerName { get; set; } = "";
+        public string PassportNumber{ get; set; } = "";
+        public string From { get; set; } = "";
+        public string To { get; set; } = "";
+        public int PassengerStatus { get; set;}
+    }
+}
